4098ffad04042d9a0763724744c3c09adc16d696
justosaenzv <justosaenzv@gmail.com>
2024-06-13 11:20:56 -0300

    agrego superfcie al tipo de dato torneo

--- models.py
+++ models.py
@@ -7,29 +7,30 @@ class Tenista():
     __tablename__ = 'tenistas'
 
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String)
+    nombre_tenista = db.Column(db.String)
     puntuacion_global = db.Column(db.Integer, db.CheckConstraint('puntuacion_global >= 0 AND puntuacion_global <= 100'))
 
 class Torneo(db.Model):
     __tablename__ = 'torneos'
 
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String)
-    tipo = db.Column(db.Enum('Grand Slam', 'Master 1000', 'ATP 500', 'ATP 250', name='tipo_valido'))
+    nombre_torneo = db.Column(db.String)
+    categoria = db.Column(db.Enum('Grand Slam', 'Master 1000', 'ATP 500', 'ATP 250', name='categoria_valida'))
+    superficie = db.Column(db.Enum('Polvo de ladrillo', 'Cemento', 'Pasto', name='superficie_valida'))
     cant_jugadores = db.Column(db.Integer)
 
     @property
     def cant_jugadores(self):
-        if self.tipo == 'Grand Slam':
+        if self.categoria == 'Grand Slam':
             return 16
-        elif self.tipo == 'Master 1000':
+        elif self.categoria == 'Master 1000':
             return 8
-        elif self.tipo == 'ATP 500':
+        elif self.categoria == 'ATP 500':
             return 4
-        elif self.tipo == 'ATP 250':
+        elif self.categoria == 'ATP 250':
             return 2
         else:
-            return 0  # or raise an exception for an unknown type
+            return 0
 
     @cant_jugadores.setter
     def cant_jugadores(self, value):
