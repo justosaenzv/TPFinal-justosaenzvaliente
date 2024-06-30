3456ac277ed4e3c7d47894881a62b4b7509a3baa
justosaenzv <justosaenzv@gmail.com>
2024-06-30 12:04:29 -0300

    Agrego funcionalidad crear_torneo y unifico rutas crear_jugador

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
 from models import Tenista, db, Torneo, Historial_torneos
+from datetime import datetime
 
 app = Flask(__name__)
 port = 5000
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/tp_tenistas'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'some_secret_key'
 
 @app.route('/', methods=['GET'])
 def homepage():
@@ -97,37 +97,81 @@ def obtener_tenista_por_nombre():
     else:
         return jsonify({'error': 'Tenista no encontrado'}), 404
 
-@app.route('/crear-jugador', methods=['GET'])
-def mostrar_formulario_jugador():
-    return render_template('crear_nuevo_jugador.html')
-
-
-@app.route('/crear-jugador', methods=['POST'])
+@app.route('/crear-jugador', methods=['GET', 'POST'])
 def crear_jugador():
-    nombre_tenista = request.form['nombre_tenista']
-    nacionalidad = request.form['nacionalidad']
-    puntuacion_global = request.form['puntuacion_global']
-    superficie_preferida = request.form['superficie_preferida']
-    altura_cm = request.form['altura_cm']
-    peso_kg = request.form['peso_kg']
+    if request.method == 'POST':
+        nombre_tenista = request.form['nombre_tenista']
+        nacionalidad = request.form['nacionalidad']
+        puntuacion_global = request.form['puntuacion_global']
+        superficie_preferida = request.form['superficie_preferida']
+        altura_cm = request.form['altura_cm']
+        peso_kg = request.form['peso_kg']
 
-    if not (0 <= int(puntuacion_global) <= 100):
-        flash('La puntuación global debe estar entre 0 y 100.')
-        return redirect(url_for('mostrar_formulario_jugador'))
+        if not (0 <= int(puntuacion_global) <= 100):
+            flash('La puntuación global debe estar entre 0 y 100.')
+            return redirect(url_for('crear_jugador'))
 
-    nuevo_tenista = Tenista(
-        nombre_tenista=nombre_tenista,
-        nacionalidad=nacionalidad,
-        puntuacion_global=puntuacion_global,
-        superficie_preferida=superficie_preferida,
-        altura_cm=altura_cm,
-        peso_kg=peso_kg
-    )
+        nuevo_tenista = Tenista(
+            nombre_tenista=nombre_tenista,
+            nacionalidad=nacionalidad,
+            puntuacion_global=puntuacion_global,
+            superficie_preferida=superficie_preferida,
+            altura_cm=altura_cm,
+            peso_kg=peso_kg
+        )
 
-    db.session.add(nuevo_tenista)
-    db.session.commit()
-    flash('Nuevo jugador creado exitosamente!')
-    return redirect(url_for('mostrar_formulario_jugador'))
+        db.session.add(nuevo_tenista)
+        db.session.commit()
+        
+        return render_template('crear_jugador.html', success=True)
+    
+    return render_template('crear_jugador.html', success=False)
+
+@app.route('/crear-torneo', methods=['GET', 'POST'])
+def crear_torneo():
+    if request.method == 'POST':
+        nombre_torneo = request.form['nombre_torneo']
+        categoria = request.form['categoria']
+        superficie = request.form['superficie']
+        
+        nuevo_torneo = Torneo(
+            nombre_torneo=nombre_torneo,
+            categoria=categoria,
+            superficie=superficie
+        )
+        db.session.add(nuevo_torneo)
+        db.session.commit()
+        
+        return render_template('crear_torneo.html', success=True)
+    
+    return render_template('crear_torneo.html', success=False)
+
+@app.route('/crear-historial-torneo', methods=['GET', 'POST'])
+def crear_historial_torneo():
+    if request.method == 'POST':
+        fecha = request.form['fecha']
+        torneo_id = request.form['torneo']
+        id_ganador = request.form['jugador_ganador']
+        
+        fecha = datetime.strptime(fecha, '%Y-%m-%d').date()
+
+        nuevo_registro = Historial_torneos(
+            fecha=fecha,
+            torneo_id=torneo_id,
+            id_ganador=id_ganador
+        )
+        db.session.add(nuevo_registro)
+        db.session.commit()
+        
+        return render_template('crear_historial.html', success=True, torneos=Torneo.query.all(), jugadores=Tenista.query.all())
+    
+    return render_template('crear_historial.html', success=False, torneos=Torneo.query.all(), jugadores=Tenista.query.all())
+
+@app.route('/')
+def index():
+    # Lógica para mostrar la página principal
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     db.init_app(app)
